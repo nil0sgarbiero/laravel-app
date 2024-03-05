df474ac949465e2e34cda2989719a395ba87fef4
nil0sgarbiero <nil0master@gmail.com>
2024-03-05 13:41:50 -0300

    Modificações 05032024

--- resources/views/dashboard/layout/app.blade.php
+++ resources/views/dashboard/layout/app.blade.php
@@ -6,13 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Dashboard {{ Auth::user()->name }}</title>
+    <tallstackui:script />
+    @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     @include('dashboard.layout.header')
-<div class="container mx-auto mt-4 py-5">
+<div class="md:container md:mx-auto mt-4 py-5">
     @yield('body')
 </div>
     @include('dashboard.layout.footer')
+    @livewireScripts
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
+/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            },
-        },
+export default {
+  presets: [
+    require('./vendor/tallstackui/tallstackui/tailwind.config.js')
+  ],
+  content: [
+    './app/**/*.php',
+    './resources/**/*.html',
+    './resources/**/*.js',
+    './resources/**/*.jsx',
+    './resources/**/*.ts',
+    './resources/**/*.tsx',
+    './resources/**/*.php',
+    './resources/**/*.vue',
+    './resources/**/*.twig',
+    './vendor/tallstackui/tallstackui/src/**/*.php',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
-    variants: {
-        extend: {
-            backgroundColor: ['active'],
-        }
-    },
-    content: [
-        './app/**/*.php',
-        './resources/**/*.html',
-        './resources/**/*.js',
-        './resources/**/*.jsx',
-        './resources/**/*.ts',
-        './resources/**/*.tsx',
-        './resources/**/*.php',
-        './resources/**/*.vue',
-        './resources/**/*.twig',
-    ],
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-    ],
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
+
